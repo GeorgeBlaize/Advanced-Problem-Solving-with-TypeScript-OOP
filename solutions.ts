0db6e0bdba7ce6ddff8e91695f72c259f445007e
GeorgeBlaize <georgepurification@gmail.com>
2026-05-08 02:08:30 +0600

    updated p4

--- solutions.ts
+++ solutions.ts
@@ -17,8 +17,12 @@ function checkType(input: StringOrNumber): string {
 }
 
 
-function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
-  return obj[key];
+function getProperty<T extends object, K extends keyof T>(
+  obj: T,
+  key: K,
+): string {
+  const value = obj[key];
+  return `"${value}";`;
 }
 
 interface Book {
